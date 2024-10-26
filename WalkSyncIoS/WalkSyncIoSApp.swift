c23ddadb2252cd2e1686fd4d5400cc7492f0930e
Abhirami007 <psabhirami015@gmail.com>
2024-10-26 14:20:36 -0400

    Initialised Walksync Ios repo

---
+++ WalkSyncIoS/WalkSyncIoSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkSyncIoSApp.swift
+//  WalkSyncIoS
+//
+//  Created by Abhirami Pradeep Susi on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct WalkSyncIoSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
